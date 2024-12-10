048add53930674ea25cb597a328753e14628ec07
Gokul <gokulplkl@gmail.com>
2024-12-10 13:47:28 -0500

    Initial Commit

---
+++ WeatherAppTPNooro/WeatherAppTPNooroApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTPNooroApp.swift
+//  WeatherAppTPNooro
+//
+//  Created by Gokul P on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppTPNooroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppTPNooroTests/WeatherAppTPNooroTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTPNooroTests.swift
+//  WeatherAppTPNooroTests
+//
+//  Created by Gokul P on 12/10/24.
+//
+
+import Testing
+@testable import WeatherAppTPNooro
+
+struct WeatherAppTPNooroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
