23f3e5b5e564994dcbdfd27421619e3564791ad6
Gokul <gokulplkl@gmail.com>
2024-12-12 00:19:38 -0500

    added views for searchView

--- WeatherAppTPNooro/Screens/HomeScreen/SearchScreenView/SearchScreenView.swift
+++ WeatherAppTPNooro/Screens/HomeScreen/SearchScreenView/SearchScreenView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct SearchScreenView: View {
-    @State var searchKey: String = ""
+
+    @State var searchKey = ""
+    @FocusState var isSearchFieldFocused: Bool
+    @State var showSearchListing = false
+
     var body: some View {
-        TextField(text: $searchKey, label: {
-            Text("Search Location")
-                .font(.system(size: 15))
-        })
+        VStack {
+            HStack {
+                TextField(text: $searchKey, label: {
+                    Text("Search Location")
+                        .font(.system(size: 15))
+                        .foregroundStyle(.textColorTertiary)
+                })
+                .focused($isSearchFieldFocused)
+                Image(systemName: isSearchFieldFocused ? "xmark.circle" : "magnifyingglass")
+                    .resizable()
+                    .frame(width: 17, height: 17)
+                    .foregroundStyle(.textColorTertiary)
+                    .onTapGesture {
+                        if isSearchFieldFocused {
+                            UIApplication.shared.sendAction(
+                                #selector(UIResponder.resignFirstResponder),
+                                to: nil,
+                                from: nil,
+                                for: nil
+                            )
+                        }
+                    }
+            }
+            .padding(16)
+            .background(RoundedRectangle(cornerRadius: 16).foregroundStyle(.boxBackground))
+            .padding(.top, 24)
+            Spacer()
+        }
+        .padding(.horizontal, 24)
+        .onChange(of: isSearchFieldFocused) {
+            print("Focus of TextField changed \(isSearchFieldFocused)")
+        }
     }
 }
 
