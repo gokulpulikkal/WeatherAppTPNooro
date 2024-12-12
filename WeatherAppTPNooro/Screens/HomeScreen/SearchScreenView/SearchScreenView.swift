8b7818ea73b6943fb57680d8403af2fb29eb7668
Gokul <gokulplkl@gmail.com>
2024-12-12 00:26:11 -0500

    SearchView changes

--- WeatherAppTPNooro/Screens/HomeScreen/SearchScreenView/SearchScreenView.swift
+++ WeatherAppTPNooro/Screens/HomeScreen/SearchScreenView/SearchScreenView.swift
@@ -11,41 +11,46 @@ struct SearchScreenView: View {
 
     @State var searchKey = ""
     @FocusState var isSearchFieldFocused: Bool
-    @State var showSearchListing = false
 
     var body: some View {
         VStack {
-            HStack {
-                TextField(text: $searchKey, label: {
-                    Text("Search Location")
-                        .font(.system(size: 15))
-                        .foregroundStyle(.textColorTertiary)
-                })
-                .focused($isSearchFieldFocused)
-                Image(systemName: isSearchFieldFocused ? "xmark.circle" : "magnifyingglass")
-                    .resizable()
-                    .frame(width: 17, height: 17)
-                    .foregroundStyle(.textColorTertiary)
-                    .onTapGesture {
-                        if isSearchFieldFocused {
-                            UIApplication.shared.sendAction(
-                                #selector(UIResponder.resignFirstResponder),
-                                to: nil,
-                                from: nil,
-                                for: nil
-                            )
-                        }
-                    }
-            }
-            .padding(16)
-            .background(RoundedRectangle(cornerRadius: 16).foregroundStyle(.boxBackground))
-            .padding(.top, 24)
+            searchTextFieldView
+                .padding(.top, 24)
             Spacer()
+            Rectangle()
+                .ignoresSafeArea()
+                .opacity(isSearchFieldFocused ? 1 : 0)
         }
         .padding(.horizontal, 24)
-        .onChange(of: isSearchFieldFocused) {
-            print("Focus of TextField changed \(isSearchFieldFocused)")
+    }
+}
+
+extension SearchScreenView {
+    var searchTextFieldView: some View {
+        HStack {
+            TextField(text: $searchKey, label: {
+                Text("Search Location")
+                    .font(.system(size: 15))
+                    .foregroundStyle(.textColorTertiary)
+            })
+            .focused($isSearchFieldFocused)
+            Image(systemName: isSearchFieldFocused ? "xmark.circle" : "magnifyingglass")
+                .resizable()
+                .frame(width: 17, height: 17)
+                .foregroundStyle(.textColorTertiary)
+                .onTapGesture {
+                    if isSearchFieldFocused {
+                        UIApplication.shared.sendAction(
+                            #selector(UIResponder.resignFirstResponder),
+                            to: nil,
+                            from: nil,
+                            for: nil
+                        )
+                    }
+                }
         }
+        .padding(16)
+        .background(RoundedRectangle(cornerRadius: 16).foregroundStyle(.boxBackground))
     }
 }
 
