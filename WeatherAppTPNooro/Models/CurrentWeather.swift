c828edb1405ee6db7ec5467c450778cdc5755aba
Gokul <gokulplkl@gmail.com>
2024-12-13 02:03:47 -0500

    weather icon blinking issue in search screen fixed

--- WeatherAppTPNooro/Models/CurrentWeather.swift
+++ WeatherAppTPNooro/Models/CurrentWeather.swift
@@ -8,16 +8,27 @@
 import Foundation
 
 struct CurrentWeather: Codable, Identifiable {
-    var id: String {
-        UUID().uuidString
-    }
-
+    
+    var id: String
     var location: Location
     var current: WeatherDetails?
+    
+    init(location: Location, current: WeatherDetails? = nil) {
+        self.id = UUID().uuidString
+        self.location = location
+        self.current = current
+    }
 
     private enum CodingKeys: String, CodingKey {
         case location
         case current
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = UUID().uuidString
+        self.location = try container.decode(Location.self, forKey: .location)
+        self.current = try container.decodeIfPresent(WeatherDetails.self, forKey: .current)
     }
 }
 
@@ -25,7 +36,7 @@ struct CurrentWeather: Codable, Identifiable {
 
 extension CurrentWeather: Equatable, Hashable {
     static func == (lhs: CurrentWeather, rhs: CurrentWeather) -> Bool {
-        lhs.location == rhs.location && lhs.current == rhs.current
+        lhs.id == rhs.id && lhs.location == rhs.location && lhs.current == rhs.current
     }
 
     func hash(into hasher: inout Hasher) {
